1a381d07e743670f280b80faeb5a2a8b44507a25
nataliefrick <natalie.salomons@gmail.com>
2022-05-12 21:11:47 +0200

    navbar start

---
+++ incl/navbar1.php
@@ -0,0 +1,37 @@
+<nav>
+    <!-- Desktop navbar -->
+    
+        <ul id="nav-desktop" class="hide-onMobile">
+            <li><a href="/cottages.php">Cottages</a></li>
+            <li><a href="/restaurant.php">Restaurant</a></li>
+            <li><a href="/activities.php">Activities</a></li>
+            <li><a href="/blog.php">Whats New</a></li>
+            <li><a href="/book.php">Book</a></li>
+        </ul>
+
+
+    <!-- Mobile Navbar Small Screen -->
+    <div class="hide-onDesktop">
+        <!-- The overlay -->
+        <div id="myNav" class="overlay">
+
+            <!-- Button to close the overlay navigation -->
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+            <!-- Overlay content -->
+            <div class="overlay-content">
+            <li><a href="/cottages.php">Cottages</a></li>
+            <li><a href="/restaurant.php">Restaurant</a></li>
+            <li><a href="/activities.php">Activities</a></li>
+            <li><a href="/blog.php">Whats New</a></li>
+            <li><a href="/book.php">Book</a></li>
+            </div>
+        </div>
+
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        <a href="javascript:void(0);" class="icon" onclick="openNav()">
+            <i class="fa fa-bars"></i>
+        </a>
+
+    </div>
+</nav>
