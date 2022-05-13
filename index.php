4659c34d9da2fd42049b1693fdaa8f7599d4bd9f
nataliefrick <natalie.salomons@gmail.com>
2022-05-13 10:39:52 +0200

    front page done

--- index.php
+++ index.php
@@ -4,17 +4,16 @@ include_once("incl/header.php");
 include_once("incl/hero.php"); ?>
 
 <!-- Title & Description Section -->
- 
 <section id="title">
     <h1 class="dont-show">Homepage Fångamon Cabin Resort</h1>
-    <h2>Come and experience the beauty of the mountains<br>
-    in style and comfort</h2>
+    <h2>Come and experience the all the forests and the mountains have to offer<br>
+    but in style and comfort</h2>
 </section>
 <!-- <div class="seperator-line"></div> -->
 
+<!-- Description of page/content -->
 <section id="description">
     <h2 class="dont-show">Description of Resort</h2>
-
     <div>
         <p><span class="big">FÅNGAMON LUXURY CABIN RESORT</span><br> is a slice of luxury in the robustness of the mountains. We have 10 cabins in different sizes to meet your needs, a main house where we offer breakfast and dinners. We also prepare take-away lunches on order. During your stay you are invited to join us for morning or evening yoga (or why not both), to start and end your day off right. During the day you can explore the area and activities provided by yourselves or book one of our guiding packages and we can lead the way.</p>
         <picture>
@@ -23,6 +22,7 @@ include_once("incl/hero.php"); ?>
     </div>
 </section>
 
+<!-- about the resort -->
 <section id="resort-sections">
     <h2 class="dont-show">What the resort has to offer</h2>
     <article class="card">
@@ -55,6 +55,65 @@ include_once("incl/hero.php"); ?>
             <a class="read-more" href="#">Read More</a>
         </div>
     </article>
+</section>
+
+<!-- Location section -->
+<section id="map">
+    <article class="content">
+        <h2>Where are we located?</h2>
+        <p class="two-columns">
+        We are located in beautiful Åre Southern Mountain range (Södra Årefjällen), which lies just southwest of Undersåker and due south of Åre. Nearby we have the ski resorts of Trillevallen, Edsåsdalen and of course Åre, just 30 km away. To the south of us we have Vålådalens elite cross country skiing training center which is open to the public. <br>
+        <span class="gps">   GPS 63°10'60.0"N, 13°07'06.4"E     or   63.2200726, 13.5735197,9.37</span>
+        </p>
+        <img src="img/map.jpg" alt="map of location">
+    </article>
+</section>
+
+<section id="picture-gallery">
+    <h2 class="dont-show">Picture Gallery</h2>
+    <picture>
+        <img src="img/details10.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details2.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details3.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details4.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details5.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details6.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details7.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details8.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details9.jpg" alt="resort details">
+    </picture>
+    <picture>
+        <img src="img/details1.jpg" alt="resort details">
+    </picture>
+</section>
+
+
+<!-- Hosts section -->
+<section id="your-hosts">
+    <h2 class="dont-show">Your hosts: </h2>
+    <div>
+        <p><span class="big">FÅNGAMON LUXURY CABIN RESORT</span> is a luxury resort located in the forests of Jämtland. Our aim is to provide a luxury get-away with maximal exposure to our beautiful nature.<br><br><span class="script">Welcome to our world!</span><br>
+        We are your hosts, Natasha and Erik.</p>
+
+        <img class="pic2" src="img/owners.jpg" alt="a closeup of the owners">
+    </div>
+    <img class="pic1" src="img/hosts.jpg" alt="a photo of the hosts">
 </section>
 
 <!-- // include("incl/footer.php");  -->
